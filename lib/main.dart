b885f7a9d2783889e4dfce233649a78ae7116a01
nam-svh <nam.svh@solu-m.com>
2025-01-02 17:50:11 +0700

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const CheckoutApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class CheckoutApp extends StatelessWidget {
+  const CheckoutApp({super.key});
 
   @override
   Widget build(BuildContext context) {
